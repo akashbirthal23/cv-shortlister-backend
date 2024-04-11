61428b3f66148fe7734c7231c73e0e4383ea3951
akashbirthal23 <akashbirthaliitk@gmail.com>
2024-04-11 17:40:46 +0530

    change concurrency

--- resume/views.py
+++ resume/views.py
@@ -10,15 +10,14 @@ from resume.Utilities.workex import get_work_experiences
 import concurrent.futures
 
 ret = []
+poll_data = []
 threshold_value_for_resume_selection = 0.0
 
 
 def index(request):
     return render(request, 'index.html')
 
-
-
-def process_projects(text, jd_text_embedding):
+def process_projects(text,jd_text_embedding,finaltext,details):
     try:
         projects = get_projects(text)
         for project in projects['Projects']:
@@ -32,14 +31,13 @@ def process_projects(text, jd_text_embedding):
             imp_text_embedding = get_embedding(imp_text)
             score = similarity_score(imp_text_embedding, jd_text_embedding)
             project['relevancy'] = score
+            finaltext.append((score, imp_text))
         projects['Projects'].sort(key=lambda x: x['relevancy'], reverse=True)
-        return projects
+        details['projects'] = projects['Projects']
     except Exception as e:
         print("Error in projects section: ", e)
 
-
-
-def process_workex(text, jd_text_embedding):
+def process_workex(text,jd_text_embedding,finaltext,details):
     try:
         work_experiences = get_work_experiences(text)
         for work_experience in work_experiences['Professional Experience']:
@@ -53,57 +51,35 @@ def process_workex(text, jd_text_embedding):
             imp_text_embedding = get_embedding(imp_text)
             score = similarity_score(imp_text_embedding, jd_text_embedding)
             work_experience['relevancy'] = score
+            finaltext.append((score, imp_text))
         work_experiences['Professional Experience'].sort(key=lambda x: x['relevancy'], reverse=True)
-        return work_experiences
+        details['Professional Experience'] = work_experiences['Professional Experience']
     except Exception as e:
         print("Error in work_experience section: ", e)
 
-
-
-def process_education(text):
+def process_education(text,details):
     try:
         education = get_education(text)
-        return education
+        details['education'] = education['College']
     except Exception as e:
         print("Error in education: ", e)
 
-
-
 def process_resume(text, jd_text_embedding, index):
 
-    resume_content = {}
-    resume_content['resume_index'] = index
+    global ret
     name = get_name(text)
-    resume_content['name'] = name
     email = get_email(text)
-    resume_content['email'] = email
 
     details = {}
     finaltext = []
 
-    futures = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        futures.append(executor.submit(process_projects, text, jd_text_embedding))
-        futures.append(executor.submit(process_workex, text, jd_text_embedding))
-        futures.append(executor.submit(process_education, text))
+    process_projects(text, jd_text_embedding, finaltext, details)
+    process_workex(text, jd_text_embedding, finaltext, details)
+    process_education(text, details)
 
-    results = []
-    for future in concurrent.futures.as_completed(futures):
-        result = future.result()
-        # print("result: ", result)
-        if 'Projects' in result:
-            details['Projects'] = result['Projects']
-            for project in details['Projects']:
-                finaltext.append((project['relevancy'], project['short_description']+project['tech_stack']))
-        elif 'Professional Experience' in result:
-            details['Professional Experience'] = result['Professional Experience']
-            for work_experience in details['Professional Experience']:
-                finaltext.append((work_experience['relevancy'], work_experience['short_description']+work_experience['tech_stack']))
-        elif 'Education' in result:
-            details['Education'] = result['Education']
 
-    resume_content['details'] = details
-    # print("details: ", details)
+    # print(details)
+
 
     imp_project_workex=""
     finaltext.sort(key=lambda x: x[0], reverse=True)
@@ -112,12 +88,15 @@ def process_resume(text, jd_text_embedding, index):
     overall_embedding = get_embedding(imp_project_workex)
     overall_score = similarity_score(overall_embedding, jd_text_embedding)
     overall_score = round(overall_score, 2)
-    resume_content['score'] = overall_score
 
+    res = []
+    if overall_score >= threshold_value_for_resume_selection:
+        ret.append({'name': name, "email": email, "score": overall_score, 'resume_index': str(index), 'details': details})
+        res.append({'name': name, "email": email, "score": overall_score, 'resume_index': str(index), 'details': details})
 
-    return resume_content
+    # print(ret)
 
-
+    return res
 
 def generate(request):
     if request.method == 'POST':
@@ -129,21 +108,22 @@ def generate(request):
         if request.FILES and "resume" in request.FILES:
             pdf_files = request.FILES.getlist("resume")
 
-            global ret
-            results = []
-            for index, pdf_file in enumerate(pdf_files):
-                content = process_pdf(pdf_file, jd_text_embedding, index)
-                if (content['score'] > threshold_value_for_resume_selection):
-                    ret.append(content)
-                    results.append(content)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+                futures = []
+                for index, pdf_file in enumerate(pdf_files):
+                    future = executor.submit(process_pdf, pdf_file, jd_text_embedding, index)
+                    futures.append(future)
+
+                # Wait for all futures to complete
+                results = []
+                for future in concurrent.futures.as_completed(futures):
+                    results.extend(future.result())
 
             return JsonResponse(results, safe=False)
         else:
             return JsonResponse({'error': 'No PDF files found in the request'}, status=400)
     else:
         return JsonResponse({'error': 'Only POST requests are supported'}, status=405)
-
-
 
 def process_pdf(pdf_file, jd_text_embedding, index):
     text = ""
@@ -153,11 +133,8 @@ def process_pdf(pdf_file, jd_text_embedding, index):
 
     return process_resume(text, jd_text_embedding, index)
 
-
-
 def poll(request):
     global ret
     poll_data = ret
     ret = []
     return JsonResponse(poll_data, safe=False)
-
